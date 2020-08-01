03db8bf696773c03121adaaba5f5c1a4268ff5cb
barteklecki <leckibartek@gmail.com>
2020-08-01 13:43:49 +0200

    feat(start): add start screen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
 
 const StartScreen = () => {
     return (
         <View style={styles.root}>
-            <Text>[ Start ]</Text>
+            <ScrollView contentContainerStyle={styles.scroll}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>PIVKO</Text>
+                    <Image
+                        style={styles.logo}
+                        resizeMode="cover"
+                        source={require('../assets/logo512.png')}
+                    />
+                    <Text>Click tab to start!</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 };
@@ -14,7 +24,25 @@ export default StartScreen;
 const styles = StyleSheet.create({
     root: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'center',
+    },
+    scroll: {
+        flexGrow: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    container: {
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        margin: 10,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        margin: 10,
     },
 });
