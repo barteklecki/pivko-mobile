f9eeb396dc65959f4c50c3b9ce91d1ca897dcaed
barteklecki <leckibartek@gmail.com>
2020-08-01 13:54:19 +0200

    feat(start): additional startscreen texts

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -6,13 +6,20 @@ const StartScreen = () => {
         <View style={styles.root}>
             <ScrollView contentContainerStyle={styles.scroll}>
                 <View style={styles.container}>
-                    <Text style={styles.title}>PIVKO</Text>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>PIVKO</Text>
+                        <Text>MOBILE</Text>
+                    </View>
                     <Image
                         style={styles.logo}
                         resizeMode="cover"
                         source={require('../assets/logo512.png')}
                     />
                     <Text>Click tab to start!</Text>
+                    <View style={styles.footer}>
+                        <Text>2020 &copy; BarLec</Text>
+                        <Text>based on PunkAPI</Text>
+                    </View>
                 </View>
             </ScrollView>
         </View>
@@ -35,14 +42,23 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
     },
+    titleContainer: {
+        margin: 10,
+        marginTop: 50,
+        alignItems: 'center',
+    },
     title: {
         fontSize: 40,
         fontWeight: 'bold',
-        margin: 10,
     },
     logo: {
         width: 200,
         height: 200,
         margin: 10,
+    },
+    footer: {
+        alignItems: 'center',
+        opacity: 0.3,
+        margin: 20,
     },
 });
