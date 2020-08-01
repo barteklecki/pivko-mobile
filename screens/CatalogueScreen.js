f078b45039e6acf5b26e594257e4a2adec26abd2
barteklecki <leckibartek@gmail.com>
2020-08-02 00:33:09 +0200

    feat(list): add API fetching

--- screens/CatalogueScreen.js
+++ screens/CatalogueScreen.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { StyleSheet, View, Button, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import axios from 'axios';
 
-import ApiData from '../models/beers';
+import { beersPerPage } from '../data/consts';
 import BeerListItem from '../components/BeerListItem';
 
 const CatalogueScreen = ({ navigation }) => {
+    const [beerList, setBeerList] = useState([]);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        if (!beerList.length && page === 1) {
+            getMeSomeBeers();
+        }
+        setPage(page + 1);
+    }, []);
+
+    const getMeSomeBeers = async () => {
+        const URL = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${beersPerPage}`;
+        const { data } = await axios(URL);
+        console.log('page=', page, ' length=', beerList.length);
+        if (data.length) {
+            setBeerList([...beerList, ...data]);
+        }
+    };
+
     return (
         <View style={styles.root}>
             <FlatList
-                data={ApiData}
+                keyExtractor={item => item.id.toString()}
+                data={beerList}
                 renderItem={({ item }) => (
                     <BeerListItem
                         name={item.name}
@@ -18,9 +39,11 @@ const CatalogueScreen = ({ navigation }) => {
                     />
                 )}
                 contentContainerStyle={styles.list}
-                ListFooterComponent={() => <Button title="Load more..." />}
                 ListFooterComponentStyle={styles.moreButton}
-                keyExtractor={item => item.id.toString()}
+                onEndReached={() => {
+                    getMeSomeBeers();
+                    setPage(page + 1);
+                }}
             />
         </View>
     );
